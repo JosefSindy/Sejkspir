0d9abb77816f148cbe73bf8e187dcf045511ebd8
unknown <josef.sindelar70@seznam.cz>
2025-11-09 21:52:59 +0100

    struktura projektu

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sejkspir</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+
+    <div class="menu">
+        <div class="menu__logo">
+            <img src="" alt="Logo">
+            <h2>Prodej shaků</h2>
+        </div>
+        <div class="menu__text">
+            <a href="onas.php"><h2>O nás</h2></a>
+            <a href="nabidka.php"><h2>Nabídka</h2></a>
+            <a href="kontakt.php"><h2>Kontakt</h2></a>
+            <a href="#"><h2>Nabídka na míru</h2></a>
+        </div>
+        <div class="menu__search">
+             <input class="search" type="text" placeholder="Vyhledej Shake..."> 🔎
+        </div>
+    </div>
+    <div class="hero">
+        <div class="hero__text">
+            <h1>Doporučené Shaky</h1>
+        </div>
+        <div class="hero__products">
+            
+            <div class="hero__products__img1">
+                <img src="" alt="Obrazek Shaku1">
+            </div>
+            <div class="hero__products__text1">
+                <p>Popis Shaku</p>
+            </div>
+
+        </div>
+
+        
+    </div>
+
+<footer>
+  <p>© Šejkspír, s. r. o.</p>
+</footer>
+
+
+</body>
+</html>
